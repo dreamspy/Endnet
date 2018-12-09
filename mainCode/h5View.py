fcdd6a8b904b89c75d26f93e9947fdf6c95265cc
Unknown <laukur@gmail.com>
2018-12-09 11:51:32 +0000

    hdf5 viewer now interactive

--- mainCode/h5View.py
+++ mainCode/h5View.py
@@ -3,25 +3,107 @@ import h5py
 import time
 import os
 import numpy as np
+import signal
+
+def db(t,v):
+    if debug == True:
+        print(t,v)
+debug = False
+debug = True
+
+def Db(t,v):
+    if Debug == True:
+        print(t,v)
+Debug = False
+Debug = True
 
 def loadDset(fileName, dataSetName):
     f = h5py.File(fileName, 'a')
     return f[dataSetName]
-########################
-#
-#   Parameters
-#
-########################
 
-fileName = 'AllStates_7-int-Vec.hdf5'
-dataSetName = "36KPvKPP"
+def loadDataSet(f):
+    ####################
+    #
+    #   Signal Handler
+    #
+    ####################
+    def exit_gracefully(signum, frame):
+        # restore the original signal handler as otherwise evil things will happen
+        # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+        signal.signal(signal.SIGINT, original_sigint)
 
-f = h5py.File(fileName, 'a')
-print("File loadded, f = " + fileName)
+        try:
+            # if input("\nReally quit? (y/n)> ").lower().startswith('y'):
+            #     print("Flushing data to disk")
+            #     f.close()
+            #     sys.exit(1)
+            print("\nFlushing data to disk")
+            f.close()
 
-action = input("Load default dataset: " + dataSetName + "? [y/n] ")
-if action == 'y' or action == 'Y':
-    dset = f[dataSetName]
-    print("loaded to dset")
+        except KeyboardInterrupt:
+            print("\nOk ok, quitting")
+            print("Flushing data to disk")
+            f.close()
+            sys.exit(1)
+
+        # restore the exit gracefully handler here
+        signal.signal(signal.SIGINT, exit_gracefully)
+
+    # store the original SIGINT handler
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+    ####################
+    #
+    #   Main
+    #
+    ####################
+
+    # f = h5py.File(fileName, 'a')
+    # print("File loadded, f = " + fileName)
+    print("Datasets:")
+    i = 0
+    datasetNames = [ds for ds in f]
+    for ds in datasetNames:
+        print('Nr:' , i , '  Name:', ds.ljust(15), "Size:", f[ds].shape )
+        i += 1
+    # print(f[0])
+    while True:
+        selectDS = int(input("Select dataset: "))
+        action = input(str("Do you want to load dataset: " + datasetNames[selectDS] + "? [y/n] "))
+        if action == 'y' or action == 'Y':
+            dataset = f[datasetNames[selectDS]]
+            break
+    return dataset
+
+def help():
+
+    Db("Usage" +
+       "\n    " +
+       "Open file handle:".ljust(25) + "f = h5py.File(fileName,'a')" +
+       "\n    " +
+       "Close file handle:".ljust(25) + "f.close" +
+       "\n    " +
+       "Load dataset:".ljust(25) + "dataset = loadDataSet(f)" +
+       "\n    " +
+       "Display this help:". ljust(25) + "help()"
+       ,'')
+
+if __name__ == '__main__':
+    ########################
+    #
+    #   Parameters
+    #
+    ########################
+
+    fileName = 'AllStates_7-int-Vec.hdf5'
+    dataSetName = "36KPvKPP"
+
+    help()
+
+    db("\nOpening file:", fileName)
+    f = h5py.File(fileName, 'a')
+    dataset = 0
+    dataset = loadDataSet(f)
 
 
